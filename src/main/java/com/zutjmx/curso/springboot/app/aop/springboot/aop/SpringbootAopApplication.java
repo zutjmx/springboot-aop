c5773bbfa7900a5f049aa45672961e3bb871d178
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-01-13 15:19:37 -0600

    first commit

---
+++ src/main/java/com/zutjmx/curso/springboot/app/aop/springboot/aop/SpringbootAopApplication.java
@@ -0,0 +1,13 @@
+package com.zutjmx.curso.springboot.app.aop.springboot.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zutjmx/curso/springboot/app/aop/springboot/aop/SpringbootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zutjmx.curso.springboot.app.aop.springboot.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
