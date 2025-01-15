ba8d0fb7134cd710846763f7c63de06920c7ad77
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-01-15 12:19:13 -0600

    Enable AOP support in SaludoAspect and update logging method signature

--- src/main/java/com/zutjmx/curso/springboot/app/aop/springboot/aop/aop/SaludoAspect.java
+++ src/main/java/com/zutjmx/curso/springboot/app/aop/springboot/aop/aop/SaludoAspect.java
@@ -5,14 +5,16 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class SaludoAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* com.zutjmx.curso.springboot.app.aop.springboot.aop.service.SaludoService.saludar(..))")
-    public void loggerAntes(JoinPoint joinPoint) {
+    @Before("execution(String com.zutjmx.curso.springboot.app.aop.springboot.aop.service.SaludoService.saludar(..))")
+    public void loggerBefore(JoinPoint joinPoint) {
         String nombreMetodo = joinPoint.getSignature().getName();
         String args = joinPoint.getArgs().toString();
         logger.info("Antes de ejecutar el metodo: " + nombreMetodo + " con los argumentos: " + args);                
